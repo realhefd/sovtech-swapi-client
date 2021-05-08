ef0fd7df8a133f0fc701136006c37393f1f0e039
Philip Onyango <philip.onyango95@gmail.com>
2021-05-08 15:31:54 +0300

    Update NotFound page with the new styled components

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,39 @@
+import { StyledCardContent, StyledRoundedBtn, StyledSpan, Container } from '../components/StyledComponents';
+const styles = {
+  container: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: '4rem'
+  },
+  title: {
+    fontSize: '12rem',
+    color: '#1e83ec',
+    fontWeight: 200
+  },
+  cardActions: {
+    justifyContent: 'space-evenly',
+    flexDirection: 'row',
+    margin: '2rem'
+  },
+  details: {
+    marginBottom: '4rem'
+  }
+} as const;
+
 const NotFound: React.FC<{}> = () => {
-  return <>Page Not Found</>;
-}
+  return (
+    <Container style={styles.container}>
+      <StyledSpan style={styles.title}>404</StyledSpan>
+
+      <StyledSpan style={styles.details}>The person or page that you are looking for was not found</StyledSpan>
+      <p>Please use one of the links below</p>
+
+      <StyledCardContent style={styles.cardActions}>
+        <StyledRoundedBtn>Back</StyledRoundedBtn>
+        <StyledRoundedBtn>Home</StyledRoundedBtn>
+      </StyledCardContent>
+    </Container>
+  );
+};
 
 export default NotFound;
