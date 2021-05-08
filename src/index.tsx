edd7e838d0669d9404ce0fd1b6408f383fa484ff
Philip Onyango <philip.onyango95@gmail.com>
2021-05-08 19:57:41 +0300

    Wrap the application with apollo and Redux providers. Pass apollo client to the provider. Change app title

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import App from './App';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_API_ENDPOINT || 'https://ngu3u.sse.codesandbox.io/',
+}) as any
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +37,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+document.title = 'Sovtech SWAPI Test';
