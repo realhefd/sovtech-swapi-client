783a082975b5fe1291a0b8f0ab8d23626d480501
Philip Onyango <philip.onyango95@gmail.com>
2021-05-08 17:27:12 +0300

    Refactore code

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import React, { lazy, Suspense } from 'react';
-import Navbar from './components/Navbar'
-import Loader from './components/Loader'
+import Navbar from './components/Navbar';
+import Loader from './components/Loader';
 
 const NotFoundPage = lazy(() => import('./pages/NotFound'));
 const DetailsPage = lazy(() => import('./pages/Details'));
 const HomePage = lazy(() => import('./pages/Home'));
 
 const App: React.FC = () => {
+
   return (
     <div style={{ textAlign: 'center' }}>
       <Router>
         <Navbar />
+
         <Suspense fallback={<Loader/>}>
           <Switch>
             <Route exact path="/details/:name" component={DetailsPage} />
