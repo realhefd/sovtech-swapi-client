9a95ae89e2828a2320b54aa47e949850f7a9547a
Philip Onyango <philip.onyango95@gmail.com>
2021-05-08 13:09:17 +0300

    Import theme switch component into navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import starwarsLogo from '../assets/starwars.jpg';
 import sovtechLogo from '../assets/logo.png';
 import { Link } from "react-router-dom";
+import ThemeSwitch from './Switch';
 import '../styles/navigation.module.css';
 
 const Navbar: React.FC<{}> = () => {
@@ -10,6 +11,7 @@ const Navbar: React.FC<{}> = () => {
         <img src={sovtechLogo} className="logo" alt="logo" />
       </Link>
       <img src={starwarsLogo} className="logo" alt="StarWars" />
+      <ThemeSwitch />
     </header>
   );
 };
