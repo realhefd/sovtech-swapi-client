ab75c8e14aeda4f0249645ba752d62257c9feb2c
Philip Onyango <philip.onyango95@gmail.com>
2021-05-08 19:54:28 +0300

    Create GraphQL queries for querying resources from the apollo server

---
+++ src/graphqlq/queries.ts
@@ -0,0 +1,36 @@
+import { gql } from "@apollo/client";
+// GraphQL queries
+export const GET_ALL_PEOPLE: string | string[] | any = gql`
+query ($page: Int!) {
+  getPeople(page: $page) {
+    count
+    next
+    previous
+    results {
+      name
+      height
+      mass
+      gender
+      homeworld {
+      }
+    }
+  }
+};
+`
+export const GET_PERSON_BY_NAME: string | string[] | any = gql`
+query ($name: String!) {
+  getPerson(name: $name) {
+    count
+    next
+    previous
+    results {
+      name
+      height
+      mass
+      gender
+      homeworld {
+      }
+    }
+  }
+}
+`;
